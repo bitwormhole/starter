841f11f4e79395ace3e62c0b20b0583e181ec253
xukun <xukun17@sina.com>
2021-06-03 21:12:45 +0800

    update: markup.Component

--- markup/component.go
+++ markup/component.go
@@ -1,9 +1,10 @@
 package markup
 
 // Component  是一个标记，用于配置一个组件
-//  请在配置组件的 struct {} 里面包含以下两个字段
-//	component  *foo.Bar
-//	context    application.Context
+// 请在配置组件的 struct {} 里面包含以下3个字段
+//    markup.Component                   // 必须的
+//    instance    *foo.Bar               // 必须的
+//    context     application.Context    // 可选的
 type Component struct {
 	// this is a empty struct
 }
