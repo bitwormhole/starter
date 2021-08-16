bb15dc231de1029ddc5a5b091c7061590abfd30f
xukun <xukun17@sina.com>
2021-08-16 13:00:09 +0800

    添加了函数：tests.ContextForApp()

--- tests/tester_context.go
+++ tests/tester_context.go
@@ -1,5 +1,11 @@
 package tests
 
-// TesterContext 是测试上下文
-type TesterContext struct {
+import "github.com/bitwormhole/starter/application"
+
+// ContextForApp 函数为 application.Context 创建一个 TestContext
+func ContextForApp(ac application.Context) TestContext {
+	tc := &DefaultTestContext{}
+	tc.Init()
+	tc.MyRunner = &DefaultRunner{}
+	return tc
 }
