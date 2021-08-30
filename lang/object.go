e41c289c08304437d866f20a5316cc0f24d8b60d
xukun <xukun17@sina.com>
2021-08-30 15:22:28 +0800

    添加了“lang.BaseObject”和“lang.Serializable”.

--- lang/object.go
+++ lang/object.go
@@ -1,4 +1,12 @@
 package lang
 
+// Object 对象：相当于 interface{}
 type Object interface {
+}
+
+// BaseObject 基本对象：比Object更复杂一点点
+type BaseObject interface {
+	Stringer
+	Equals(other BaseObject) bool
+	HashCode() int
 }
---
+++ lang/serializable.go
@@ -0,0 +1,6 @@
+package lang
+
+// Serializable 一个标记接口：表示对象可以序列化
+type Serializable interface {
+	Stringer
+}
