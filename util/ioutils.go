466c1541a838eafd88d8449092613491e1d1c10f
xukun <xukun17@sina.com>
2021-10-28 20:42:09 +0800

    fix: bug in util.PumpStream()

--- util/ioutils.go
+++ util/ioutils.go
@@ -14,19 +14,21 @@ func PumpStream(in io.Reader, out io.Writer, buffer []byte) error {
 	}
 
 	for {
-		n1, err := in.Read(buffer)
-		if err != nil {
-			if "EOF" == strings.ToUpper(err.Error()) {
+		n1, err1 := in.Read(buffer)
+		if n1 > 0 {
+			n2, err2 := out.Write(buffer[0:n1])
+			if err2 != nil {
+				return err2
+			}
+			if n1 != n2 {
+				return errors.New("len(read) != len(write)")
+			}
+		}
+		if err1 != nil {
+			if "EOF" == strings.ToUpper(err1.Error()) {
 				break
 			}
-			return err
-		}
-		n2, err := out.Write(buffer[0:n1])
-		if err != nil {
-			return err
-		}
-		if n1 != n2 {
-			return errors.New("len(read) != len(write)")
+			return err1
 		}
 	}
 
