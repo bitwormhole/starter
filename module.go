4ffffcc27df1c4e87313ef20411f609c69011753
xukun <xukun17@sina.com>
2021-11-10 20:39:32 +0800

    update to v0.0.79

--- module.go
+++ module.go
@@ -10,8 +10,8 @@ import (
 
 const (
 	myName     = "github.com/bitwormhole/starter"
-	myVersion  = "v0.0.78"
-	myRevision = 78
+	myVersion  = "v0.0.79"
+	myRevision = 79
 )
 
 //go:embed src/main/resources
