a2467de8a6dd44449546a55af9f6c232bfe20d66
比特虫洞 <43868880+bitwormhole@users.noreply.github.com>
2022-01-21 14:58:37 +0800

    update to v0.1.3

--- module.go
+++ module.go
@@ -10,8 +10,8 @@ import (
 
 const (
 	myName     = "github.com/bitwormhole/starter"
-	myVersion  = "v0.1.2"
-	myRevision = 85
+	myVersion  = "v0.1.3"
+	myRevision = 86
 )
 
 //go:embed src/main/resources
