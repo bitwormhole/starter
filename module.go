56bff7ef4390ca982e8e00c464876d6ae44517bf
MEZ <mez@bitwormhole.com>
2023-01-16 12:31:49 +0800

    update to v0.1.6

--- module.go
+++ module.go
@@ -10,8 +10,8 @@ import (
 
 const (
 	myName     = "github.com/bitwormhole/starter"
-	myVersion  = "v0.1.5"
-	myRevision = 88
+	myVersion  = "v0.1.6"
+	myRevision = 89
 )
 
 //go:embed src/main/resources
